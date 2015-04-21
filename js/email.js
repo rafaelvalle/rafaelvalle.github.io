5d64f50db52da79eb078b002389e67c2f6f09575
Rafael Valle <jrafaelvalle@gmail.com>
2015-04-20 18:46:51 -0700

    adding github website for komposition

---
+++ js/email.js
@@ -0,0 +1,4 @@
+function email(jrafaelvalle){
+    var email = new String("jrafaelvalle@gmail.com");
+      return email;
+} 
---
+++ js/toggleMe.js
@@ -0,0 +1,14 @@
+function toggleMe(a){
+	
+    var e=document.getElementById(a);
+	
+    if(!e)return true;
+	
+    if(e.style.display=="none"){
+        e.style.display="block"
+    } 
+    else{
+        e.style.display="none"
+    }
+    return true;
+} 
